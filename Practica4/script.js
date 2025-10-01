a5582ecb0320638c4f5909bcef258d915a07293f
MELISSA JAZMIN TORRES MARTINEZ <2430017@upv.edu.mx>
2025-09-30 22:56:37 -0600

    Add practica 4 Terminada

--- Practica4/script.js
+++ Practica4/script.js
@@ -1,25 +1,28 @@
 const form = document.getElementById("registro");
-let usuarios = JSON.parse(localStorage.getItem("usuarios")) || []; 
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) || []; //arreglo de usuarios guardado en localstorage
 
-form.addEventListener("submit", e =>{
-    e.preventDefault();
+form.addEventListener("submit", e =>{ //agregamos un evento que se ejecute cuando el formulario se envia
+    e.preventDefault(); //evita que recargue la pagina al enviar el formulrio
     
+
+    //aqui obtenemos los valores que ingresamos en los inputs del html
     const nombre = document.getElementById("nombre").value.trim();
     const correo = document.getElementById("correo").value.trim();
     const contraseña = document.getElementById("contraseña").value.trim();
 
-    if(!nombre || !correo || !contraseña){
+    if(!nombre || !correo || !contraseña){ //verificamos que todos los campos esten rellenos
         alert("Por favor ingresa todos los campos");
         return;
     }
 
-    const usuario = { correo, contraseña };
+    const usuario = { correo, contraseña }; //creamos un objeto con los datos del usuario
 
-    usuarios.push(usuario);
+    usuarios.push(usuario);//agregamos nuevo usuario al arreglo
 
+//guaradmos el arreglo actualizado en localstorgae 
     localStorage.setItem("usuarios", JSON.stringify(usuarios)); // guardamos en la misma clave
 
-    form.reset();
+    form.reset();//limpiamos el formualrio para que quede sin nada
 
-    window.location.href = "inicio.html";
+    window.location.href = "inicio.html"; //aqui estoy redirigiendo al inicio.html (inicio sesion)
 });
--- Practica4/scriptdos.js
+++ Practica4/scriptdos.js
@@ -1,5 +1,5 @@
 const formu = document.getElementById("inicio"); 
-let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];//arreglo de usuario guardado en local storage
 
 formu.addEventListener("submit", e =>{
     e.preventDefault();
@@ -7,18 +7,16 @@ formu.addEventListener("submit", e =>{
     const UCorreo = document.getElementById("correo").value.trim();
     const UContraseña = document.getElementById("contraseña").value.trim();
 
+    //verificamos que los campos esten rellenos
     if(!UCorreo || !UContraseña){
         alert("Por favor rellena los campos");
         return;
     }
-
-    const usuarioValido = usuarios.find(u => u.correo === UCorreo && u.contraseña === UContraseña);
-
+ //buscamos en el arreglo de usuario si existe un usuario con el correoy la contraseña ingresas
+    const usuarioValido = usuarios.some(u => u.correo === UCorreo && u.contraseña === UContraseña);
+ 
+    //si se encuentra un usuario valido, mostraremos un msj de bienvenida 
     if(usuarioValido){
-
-        const guardamos = {correo: usuarioValido.correo};
-
-        localStorage.setItem("guardamos", JSON.stringify(guardamos))
-        window.location.href = "nuevo.html"
+        alert("Bienvenido " + UCorreo);
     } 
 });
