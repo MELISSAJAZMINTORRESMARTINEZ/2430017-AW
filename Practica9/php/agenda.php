18ab3e2853eaf432dcdccb82b4c2a42faa23be46
MELISSA JAZMIN TORRES MARTINEZ <2430017@upv.edu.mx>
2025-11-24 21:31:02 -0600

    Implementacion del completa del Modulo Agenda

--- Practica9/php/agenda.php
+++ Practica9/php/agenda.php
@@ -3,8 +3,8 @@
 $host = "localhost";
 $port = "3306";
 $dbname = "clinica";
-$user = "root";
-$pass = "";
+$user = "admin";
+$pass = "ca99bc649c71b2383154550b34e52d0bb17fe7183054c554"; // vacio
 
 // iniciamos un bloque try para capturar errores
 try {
