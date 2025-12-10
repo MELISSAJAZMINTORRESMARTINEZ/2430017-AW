f2350c0093aa4f5520f618f216e6445fb223771d
MELISSA JAZMIN TORRES MARTINEZ <2430017@upv.edu.mx>
2025-12-10 07:38:07 -0600

    pruebita

--- Recuperacion/index.php
+++ Recuperacion/index.php
@@ -8,17 +8,20 @@
 </head>
 <body>
 
-<h2>Agregar Usuario</h2>
+<h2>Agregar Libro</h2>
 
 <form id="formCrear">
-    Nombre del usuario: <input type="text" name="nombre" id="nombre" required>
-    Email: <input type="email" name="email" id="email" required>
-    <button type="submit">Crear</button>
+    Nombre del Libro: <input type="text" name="nombre" id="nombre" required>
+        Autor: <input type="text" name="autor" id="autor" required>
+        Categeoria: <input type="text" name="categoria" id="categoria" required>
+        Paginas : <input type="number" name="paginas" id="paginas" required>
+        Editorial : <input type="text" name="editorial" id="editorial" required>
+        <button type="submit">Crear</button>
 </form>
 
 <hr>
 
-<h2>Lista de Usuarios</h2>
+<h2>Lista de Libros</h2>
 <div id="lista"></div>
 
 <script src="script.js"></script>
